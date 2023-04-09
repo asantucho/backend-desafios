9272e7903ea5fd9f189f66e32607e973bbf43cb5
Maria Agustina Santucho <asantucho35@gmail.com>
2023-04-09 19:21:30 -0300

    desafio entregable 01. Clase ProductManager con metodos agregar producto, obtener productos y obtener producto por identificador

---
+++ desafio-01.js
@@ -0,0 +1,51 @@
+// Desafio entregable N°1 - BACKEND - CODERHOUSE
+
+class ProductManager {
+  constructor() {
+    this.products = [];
+  }
+  addProduct(title, description, price, thumbnail, stock) {
+    const product = {
+      title,
+      description,
+      price,
+      thumbnail,
+      id: this.#newId() + 1,
+      stock,
+    };
+    this.products.push(product);
+  }
+  #newId() {
+    let initialId = 0;
+    this.products.map((product) => {
+      if (product.id > initialId) initialId = product.id;
+    });
+    return initialId;
+  }
+  getProducts() {
+    return this.products;
+  }
+  getProductById(productId) {
+    return this.products.find((product) => product.id === productId);
+  }
+}
+
+const productManager = new ProductManager();
+productManager.addProduct(
+  'lipstick',
+  'red lipstick with matte finish',
+  19.99,
+  'https://www.lancome-usa.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-lancome-us-Library/default/dwf59fb71d/images/beauty%20mag/2022/11-November/LU13538_LIPSTICK_ON_LIP_1280x639.jpg?sw=1350&sh=674&sm=cut&q=70',
+  18
+);
+console.log(productManager);
+productManager.addProduct(
+  'eyeliner',
+  'enhance your look with this lasting liquid eyeliner',
+  15.99,
+  'https://cdn.shopify.com/s/files/1/0483/3588/0360/products/Product-Images_Swatches_LiquidEyeliner_720x.jpg?v=1666128024',
+  24
+);
+console.log(productManager);
+console.log(productManager.getProducts());
+console.log(productManager.getProductById(2));
